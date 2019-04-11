6381311bfa8b48823d156f13f6f33f6b65b6d10f
Darren Hall <itsdarrenhall@gmail.com>
2019-04-11 09:39:39 +0100

    feat: update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Darren Hall`,
+    description: `Freelance frontend developer based in Manchester`,
+    author: `frontendbeast`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
