7cc67eb1608025fe5668ec73821ad87fa6f1447b
Darren Hall <itsdarrenhall@gmail.com>
2019-04-11 09:41:06 +0100

    feat: remove unnecessary metadata from header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,36 +1,22 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <main>{children}</main>
-        <footer class="c-section c-section--masthead">
-          <div class="o-wrapper">
-            <div class="o-grid">
-              <div class="o-grid__item">
-                <p>&copy; {new Date().getFullYear()} Darren Hall | <a href="https://github.com/frontendbeast/darrenhall.co">View source code on GitHub</a> | <a href="https://twitter.com/frontendbeast">Follow me on Twitter</a></p>
-              </div>
-            </div>
+  <>
+    <Header />
+    <main>{children}</main>
+    <footer className="c-section c-section--masthead">
+      <div className="o-wrapper">
+        <div className="o-grid">
+          <div className="o-grid__item">
+            <p>&copy; {new Date().getFullYear()} Darren Hall | <a href="https://github.com/frontendbeast/darrenhall.co">View source code on GitHub</a> | <a href={`https://twitter.com/frontendbeast`}>Follow me on Twitter</a></p>
           </div>
-        </footer>
-      </>
-    )}
-  />
+        </div>
+      </div>
+    </footer>
+  </>
 )
 
 Layout.propTypes = {
