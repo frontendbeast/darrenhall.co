33865ab9ccbf7c831fcba3765aa9a155f82383ee
Darren Hall <itsdarrenhall@gmail.com>
2019-04-11 09:42:00 +0100

    feat: update content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { StaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -6,43 +7,58 @@ import SEO from "../components/seo"
 import "../sass/main.scss"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <main class="c-section">
-      <div class="o-wrapper">
-        <h1 class="u-visually-hidden">Darren Hall</h1>
-        <h2>I&rsquo;m a freelance frontend developer based in Manchester.</h2>
-        <p>With over {new Date().getFullYear() - 2004} years of commercial experience I develop responsive, performant and accessible websites using HTML, CSS and JavaScript.</p>
-        <p>I work extensively with Gulp and Grunt to automate and optimise build processes. I frequently use Travis CI and have also used Jenkins and TeamCity for continuous integration and deployment. </p>
-        <h3>Skills</h3>
-        <div class="o-grid">
-          <div class="o-grid__item o-grid__item--1/1 o-grid__item--1/2@md">
-            <ul>
-              <li>CSS, Sass and LESS</li>
-              <li>HTML and PHP</li>
-              <li>JS, Node and jQuery</li>
-            </ul>
-          </div>
-          <div class="o-grid__item o-grid__item--1/1 o-grid__item--1/2@md">
-            <ul>
-              <li>Node, Gulp and Grunt</li>
-              <li>React, Angular and Ionic</li>
-              <li>CI and TDD</li>
-            </ul>
-          </div>
-        </div>
-        <h3>Test Driven Development</h3>
-        <p>Mocha and Chai are my go to test tools, but I have also used Jasmine and Karma. I have some experience using BackstopJS to create automated visual regression tests.</p>
-        <h3>Experienced with WordPress and Craft CMS</h3>
-        <p>I have previously worked with extensively with WordPress, and more recently with Craft CMS. My previous work as an Object-Oriented PHP developer with CakePHP, CodeIgniter and Symfony allows me to get the most out of these systems.</p>
-        <h3>Sitecore and Umbraco&nbsp;friendly</h3>
-        <p>Many of the projects I have worked on in the last few years used a .NET CMS, and I am skilled at working with both Sitecore and Umbraco. I am adept at integrating Razor templates and have a working knowledge of editing controllers and tests.</p>
-        <h2>Currently available from May</h2>
-        <p>If you&rsquo;re interested in working with me then I&rsquo;d love to hear from you! I&rsquo;m currently available from the middle of May 2019. </p>
-        <p><a href="mailto:darren@darrenhall.co" class="c-button">Get in touch</a></p>
-      </div>
-    </main>
-  </Layout>
+  <StaticQuery
+    query={graphql`
+      query siteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Layout>
+          <SEO title={data.site.siteMetadata.title} keywords={[`freelance`, `frontend`, `front end`, `front-end`, 'developer', `Manchester`, 'remote', `contractor`]} />
+          <main className="c-section">
+            <div className="o-wrapper">
+              <h1 className="u-visually-hidden">Darren Hall</h1>
+              <h2>I&rsquo;m a freelance frontend developer based in Manchester.</h2>
+              <p>With over {new Date().getFullYear() - 2004} years of commercial experience I develop responsive, performant and accessible websites using HTML, CSS and JavaScript. I’ve done a lot of React work recently and have also done a fair few Node, Gatsby and Jekyll sites.</p>
+              <p>I use Webpack in my build automation and have previously used Gulp and Grunt extensively. I’m currently moving my deployment from Travis CI to Buddy. </p>
+              <h3>Skills</h3>
+              <div className="o-grid">
+                <div className="o-grid__item o-grid__item--1/1 o-grid__item--1/2@md">
+                  <ul>
+                    <li>CSS and Sass</li>
+                    <li>HTML</li>
+                    <li>JS, ES5 and ES6+</li>
+                  </ul>
+                </div>
+                <div className="o-grid__item o-grid__item--1/1 o-grid__item--1/2@md">
+                  <ul>
+                    <li>Node, Webpack, Gulp</li>
+                    <li>React, Jekyll, Gatsby and Angular </li>
+                    <li>CI and TDD</li>
+                  </ul>
+                </div>
+              </div>
+              <h3>Test Driven Development</h3>
+              <p>Mocha and Chai are my go to test tools, but I have also used Jasmine and Karma. I have some experience using BackstopJS to create automated visual regression tests.</p>
+              <h3>Experienced with WordPress and Craft CMS</h3>
+              <p>I have previously worked with extensively with WordPress, and more recently with Craft CMS. My previous work as an Object-Oriented PHP developer with CakePHP, CodeIgniter and Symfony allows me to get the most out of these systems.</p>
+              <h3>Sitecore and Umbraco&nbsp;friendly</h3>
+              <p>Many of the projects I have worked on in the last few years used a .NET CMS, and I am skilled at working with both Sitecore and Umbraco. I am adept at integrating Razor templates and have a working knowledge of editing controllers and tests.</p>
+              <h2>Currently available from May</h2>
+              <p>If you&rsquo;re interested in working with me then I&rsquo;d love to hear from you! I&rsquo;m currently available from the start of May 2019. </p>
+              <p><a href="mailto:darren@darrenhall.co" className="c-button">Get in touch</a></p>
+            </div>
+          </main>
+        </Layout>
+      </>
+    )}
+  />
 )
 
 export default IndexPage
